c9f53aebd218216e344dc11c4ebcede3d634ec17
Yuya <ytnkgt@gmail.com>
2023-01-21 16:08:11 +0900

    fixed bug

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 script_home=$(cd $(dirname $0) && pwd)
 backup_folder="$script_home/backup"
-if [ $ostype == "msys" ] ; then
-    code_path="$userprofile/appdata/roaming/code/user"
+if [ "$OSTYPE" == "msys" ] ; then
+    code_path="$USERPROFILE/appdata/roaming/code/user"
     echo $code_path
 fi
+
+echo "mv $code_path/settings.json $backup_folder"
 mv "$code_path/settings.json" "$backup_folder"
 cp "$script_home/settings.json" "$code_path/settings.json"
 mv "$code_path/keybindings.json" "$backup_folder"
-cp "$script_home/keybindings.json" "$CODE_PATH/keybindings.json"
+cp "$script_home/keybindings.json" "$code_path/keybindings.json"
